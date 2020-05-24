c685207b5dd92ec90a0c7b0e40b36c4fe4b6d99c
MphoKhotleng <mphokhotleng@gmail.com>
2020-05-24 14:35:27 +0200

    updating code

--- src/ErrorCalculator.py
+++ src/ErrorCalculator.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class ErrorCalculator:
     def __init__(self, y, y_pred):
         self.y = np.array(y)
@@ -14,11 +16,15 @@ class ErrorCalculator:
     
     def get_rmse(self):
         return np.sqrt(((self.y_pred - self.y) ** 2).mean())
+
+    def get_mae(self):
+        return np.mean(np.abs((self.y - self.y_pred) / self.y))
     
     def error_summary(self):
         print(f'average standard residuals: {self.get_standardised_residuals().mean()}')
         print(f'min standard residuals: {min(self.get_standardised_residuals())}')
         print(f'maximum standard residuals: {max(self.get_standardised_residuals())}')
         print(f'MSE: {self.get_mse()}')
+        print(f'MAE: {self.get_mae()}')
         print(f'RSME: {self.get_rmse()}')
 
--- src/Plotter.py
+++ src/Plotter.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 class Plotter():
     def __init__(self, y, y_pred):
         self.y = y
@@ -16,15 +19,35 @@ class Histogram_Plotter(Plotter):
     def __init__(self, y, y_pred):
         Plotter.__init__(self, y, y_pred)
 
+
+    def plot(self):
+        f, axes = plt.subplots(1, 2,figsize=(15, 5))
+        grid = plt.GridSpec(1, 2, wspace=0.5, hspace=0.3)
+        plt.subplot(grid[0, 0])
+        sns.distplot((self.y - self.y_pred))
+        plt.title('Distribution of Residuals')
+
+        plt.subplot(grid[0, 1])
+        sns.distplot((self.y - self.y_pred))
+        plt.title('Distribution of Residuals')
+        return plt.show()
+
+class Distplot(Plotter):
+    def __init__(self, y, y_pred):
+        Plotter.__init__(self, y, y_pred)
+
+
 class Scatter_Plot(Plotter):
     def __init__(self, y, y_pred):
         Plotter.__init__(self, y, y_pred)
+
 
     def plot(self):
         df = pd.DataFrame({"Actual": self.y, 
                             "Predicted": self.y_pred})
         df.plot.scatter(x="Actual", y="Predicted")
         plt.title("Predicted vs Actual Values")
-        plt.xlabel("Actual")
-        plt.ylabel("Prediction")
         return plt.show()
+
+
+

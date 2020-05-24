87ab6e2106442c130d265f8a5a4172925cabb69c
MphoKhotleng <mphokhotleng@gmail.com>
2020-05-24 17:25:44 +0200

    ready for review

--- src/Plotter.py
+++ src/Plotter.py
@@ -2,51 +2,69 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 class Plotter():
-    def __init__(self, y, y_pred):
+    def __init__(self, y, y_pred, y2, y_pred2):
         self.y = y
         self.y_pred = y_pred
-        
+        self.y2 = y2
+        self.y_pred2 = y_pred2
     def run_calcs(self):
         return self.y - self.y_pred
     
-    def plot(self):
-        plt.hist(self.y - self.y_pred)
-        plt.xlabel('residuals')
-        plt.ylabel('frequency')
-        return plt.show()
 
 class Histogram_Plotter(Plotter):
-    def __init__(self, y, y_pred):
-        Plotter.__init__(self, y, y_pred)
+    def __init__(self, y, y_pred, y2, y_pred2):
+        Plotter.__init__(self, y, y_pred, y2, y_pred2)
+
+    def plot(self):
+        f, axes = plt.subplots(1, 2,figsize=(15, 5))
+        grid = plt.GridSpec(1, 2, wspace=0.5, hspace=0.3)
+        plt.subplot(grid[0, 0])
+        plt.hist(self.y - self.y_pred)
+        plt.title('Model 1: Histogram of Residuals', fontsize= 15)
+        plt.xlabel('Residuals')
+        plt.ylabel('Frequency')
+
+        plt.subplot(grid[0, 1])
+        plt.hist(self.y2 - self.y_pred2, color="tan")
+        plt.title('Model 2', fontsize= 15)
+        plt.xlabel('Residuals')
+        plt.ylabel('Frequency')
+        return plt.show()
+
+
+    
+class Distplot(Plotter):
+    def __init__(self, y, y_pred, y2, y_pred2):
+        Plotter.__init__(self, y, y_pred, y2, y_pred2)
+
+    def plot(self):
+        f, axes = plt.subplots(1, 2,figsize=(15, 5))
+        grid = plt.GridSpec(1, 2, wspace=0.5, hspace=0.3)
+        plt.subplot(grid[0, 0])
+        sns.distplot((self.y - self.y_pred), color="black")
+        plt.title('Model 1: Distribution of Residuals')
+        
+#DON'T FORGET TO LABEL AXES  
+        plt.subplot(grid[0, 1])
+        sns.distplot((self.y2 - self.y_pred2), color="purple")
+        plt.title('Model 2')
+        return plt.show()
+
+
+class Scatter_Plot(Plotter):
+    def __init__(self, y, y_pred, y2, y_pred2):
+        Plotter.__init__(self, y, y_pred, y2, y_pred)
 
 
     def plot(self):
         f, axes = plt.subplots(1, 2,figsize=(15, 5))
         grid = plt.GridSpec(1, 2, wspace=0.5, hspace=0.3)
         plt.subplot(grid[0, 0])
-        sns.distplot((self.y - self.y_pred))
-        plt.title('Distribution of Residuals')
-
+        plt.scatter(self.y, self.y_pred, color="pink")
+        plt.title("Model 1: Actual vs Predicted")
         plt.subplot(grid[0, 1])
-        sns.distplot((self.y - self.y_pred))
-        plt.title('Distribution of Residuals')
-        return plt.show()
-
-class Distplot(Plotter):
-    def __init__(self, y, y_pred):
-        Plotter.__init__(self, y, y_pred)
-
-
-class Scatter_Plot(Plotter):
-    def __init__(self, y, y_pred):
-        Plotter.__init__(self, y, y_pred)
-
-
-    def plot(self):
-        df = pd.DataFrame({"Actual": self.y, 
-                            "Predicted": self.y_pred})
-        df.plot.scatter(x="Actual", y="Predicted")
-        plt.title("Predicted vs Actual Values")
+        plt.scatter(self.y2, self.y_pred2, color="grey")
+        plt.title("Model 2")
         return plt.show()
 
 

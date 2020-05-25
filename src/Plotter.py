3a94027a0f375773b234cabd2c42fd49aad8d19a
MphoKhotleng <mphokhotleng@gmail.com>
2020-05-25 04:08:53 +0200

    used super() as advised

--- src/Plotter.py
+++ src/Plotter.py
@@ -13,20 +13,20 @@ class Plotter():
 
 class Histogram_Plotter(Plotter):
     def __init__(self, y, y_pred, y2, y_pred2):
-        Plotter.__init__(self, y, y_pred, y2, y_pred2)
+        super().__init__(y, y_pred, y2, y_pred2)
 
     def plot(self):
         f, axes = plt.subplots(1, 2,figsize=(15, 5))
         grid = plt.GridSpec(1, 2, wspace=0.5, hspace=0.3)
         plt.subplot(grid[0, 0])
         plt.hist(self.y - self.y_pred)
-        plt.title('Model 1: Histogram of Residuals', fontsize= 15)
+        plt.title('Model 1: Histogram of Residuals', fontsize= 20)
         plt.xlabel('Residuals')
         plt.ylabel('Frequency')
 
         plt.subplot(grid[0, 1])
         plt.hist(self.y2 - self.y_pred2, color="tan")
-        plt.title('Model 2', fontsize= 15)
+        plt.title('Model 2', fontsize= 20)
         plt.xlabel('Residuals')
         plt.ylabel('Frequency')
         return plt.show()
@@ -34,26 +34,26 @@ class Histogram_Plotter(Plotter):
 
     
 class Distplot(Plotter):
-    def __init__(self, y, y_pred, y2, y_pred2):
-        Plotter.__init__(self, y, y_pred, y2, y_pred2)
+    def __init__(self, y, y_pred, y2, y_pred2): 
+        super().__init__(y, y_pred, y2, y_pred2)
 
     def plot(self):
         f, axes = plt.subplots(1, 2,figsize=(15, 5))
         grid = plt.GridSpec(1, 2, wspace=0.5, hspace=0.3)
         plt.subplot(grid[0, 0])
         sns.distplot((self.y - self.y_pred), color="black")
-        plt.title('Model 1: Distribution of Residuals')
+        plt.title('Model 1: Distribution of Residuals', fontsize= 20)
         
 #DON'T FORGET TO LABEL AXES  
         plt.subplot(grid[0, 1])
         sns.distplot((self.y2 - self.y_pred2), color="purple")
-        plt.title('Model 2')
+        plt.title('Model 2', fontsize= 20)
         return plt.show()
 
 
 class Scatter_Plot(Plotter):
     def __init__(self, y, y_pred, y2, y_pred2):
-        Plotter.__init__(self, y, y_pred, y2, y_pred)
+        super().__init__(y, y_pred, y2, y_pred2)
 
 
     def plot(self):
@@ -61,10 +61,10 @@ class Scatter_Plot(Plotter):
         grid = plt.GridSpec(1, 2, wspace=0.5, hspace=0.3)
         plt.subplot(grid[0, 0])
         plt.scatter(self.y, self.y_pred, color="pink")
-        plt.title("Model 1: Actual vs Predicted")
+        plt.title("Model 1: Actual vs Predicted", fontsize= 20)
         plt.subplot(grid[0, 1])
         plt.scatter(self.y2, self.y_pred2, color="grey")
-        plt.title("Model 2")
+        plt.title("Model 2", fontsize= 20)
         return plt.show()
 
 
